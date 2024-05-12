6594a6a55f9c24ad629161447d670228b8b87ff6
Wonkruu <williamdpe@gmail.com>
2024-05-12 16:50:41 +0200

    fix(docker): api exposed port

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,13 +2,14 @@ import fastify from 'fastify';
 import mongoose from 'mongoose';
 import {applicationController} from "./controllers/application";
 
-const app = fastify();
+const app = fastify({logger: true});
 mongoose.connect('mongodb://admin:password@database:27017')
     .then(() => {
         console.log('Database connected');
         app.register(applicationController, {prefix: '/applications'});
-        app.listen({ port: 3001 })
-            .then(() => console.log('Server is up'));
+        app.listen({ port: 3001, host: '0.0.0.0' })
+            .then(() => console.log('Server is up'))
+            .catch(error => console.log(error));
     })
     .catch((error) => {
         console.log('An error has occurred:\n', error);
